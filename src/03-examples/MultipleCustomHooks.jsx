4950d9eaf06e0a621eb4659c542b7167d104cc12
MattoDev <mateofonseca.profesional@gmail.com>
2023-01-17 21:38:15 -0500

    useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -5,9 +5,28 @@ export const MultipleCustomHooks = () => {
     "https://api.breakingbadquotes.xyz/v1/quotes/1"
   );
   console.log({ data, isLoading, hasErrror });
+
+  const { author, quote } = !!data && data[0];
+
+  /*   if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+ */
   return (
     <>
       <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary">Next quote</button>
     </>
   );
 };
