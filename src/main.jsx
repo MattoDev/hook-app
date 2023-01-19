a6630757fa71797a7699cbcab1a36a14ef36b376
MattoDev <mateofonseca.profesional@gmail.com>
2023-01-18 22:23:52 -0500

    task memorize

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,12 @@ import { FocusScreen } from "./04-useRef/FocusScreen";
 import { Layout } from "./05-useLayoutEffect/Layout";
 import { Memorize } from "./06-memo/Memorize";
 import { CallbackHook } from "./06-memo/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   </React.StrictMode>
 );
