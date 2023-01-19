1ad664be6782493fb6ed669658dea5fb59f7802c
MattoDev <mateofonseca.profesional@gmail.com>
2023-01-18 22:04:22 -0500

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,12 @@ import { SimpleForm } from "./02-useEffect/SimpleForm";
 import { FocusScreen } from "./04-useRef/FocusScreen";
 import { Layout } from "./05-useLayoutEffect/Layout";
 import { Memorize } from "./06-memo/Memorize";
+import { CallbackHook } from "./06-memo/CallbackHook";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Memorize />
+    <CallbackHook />
   </React.StrictMode>
 );
