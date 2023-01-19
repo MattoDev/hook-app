69c2492caaec189e5ef45dcc967bfae49292bcea
MattoDev <mateofonseca.profesional@gmail.com>
2023-01-18 21:12:15 -0500

    useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,33 @@
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples/index";
+
+export const Layout = () => {
+  const { counter, increment, decrement, reset } = useCounter();
+  const { data, isLoading, hasErrror } = useFetch(
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+  );
+  console.log({ data, isLoading, hasErrror });
+
+  const { author, quote } = !!data && data[0];
+
+  /*   if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+ */
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? <LoadingQuote /> : <Quote author={author} quote={quote} />}
+
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
+        Next quote
+      </button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import { CounterwithCustomHook } from "./01-useState/CounterwithCustomHook";
 import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 import { SimpleForm } from "./02-useEffect/SimpleForm";
 import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>
 );
