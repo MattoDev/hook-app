d1e560960154d7c16da87a7657d4ab409ab33cc5
MattoDev <mateofonseca.profesional@gmail.com>
2023-02-08 21:13:24 -0500

    Create Context

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes, Link } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
+import { UserProvider } from "./context/UserProvider";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
       {/* <h1>MainApp</h1> */}
       {/* <hr /> */}
 
@@ -21,6 +22,6 @@ export const MainApp = () => {
 
         <Route path="/*" element={<Navigate to={"/about"} />} />
       </Routes>
-    </>
+    </UserProvider>
   );
 };
---
+++ src/09-useContext/context/UserContext.jsx
@@ -0,0 +1,5 @@
+//Es un HOC
+
+import { createContext } from "react";
+
+export const UserContext = createContext();
