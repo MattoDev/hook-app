6697367315c4b22b54c6924f6ddf6f9c24eed4b8
MattoDev <mateofonseca.profesional@gmail.com>
2023-02-13 21:43:44 -0500

    increment, decrement and reset test

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
 
 describe("Pruebas en el use Counter", () => {
@@ -16,5 +16,42 @@ describe("Pruebas en el use Counter", () => {
     const { result } = renderHook(() => useCounter(100));
     const { counter } = result.current;
     expect(counter).toBe(100);
+  });
+
+  test("Debe  incrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { counter, increment } = result.current;
+
+    act(() => {
+      increment();
+      increment(2);
+    });
+
+    expect(result.current.counter).toBe(103);
+  });
+
+  test("Debe decrementar el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { counter, decrement } = result.current;
+
+    act(() => {
+      decrement();
+      decrement(2);
+    });
+
+    expect(result.current.counter).toBe(97);
+  });
+  test("Debe de resetear el contador", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { counter, decrement, reset, increment } = result.current;
+
+    act(() => {
+      decrement();
+      decrement(2);
+      increment(23);
+      reset();
+    });
+
+    expect(result.current.counter).toBe(100);
   });
 });
